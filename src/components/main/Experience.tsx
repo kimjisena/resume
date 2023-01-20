f4413a71ed3789a541e6339e2e15a62d009cbd4e
Kim Jisena <pjohmaths@gmail.com>
2023-01-20 17:21:39 +0300

    yeaaah

--- src/components/main/Experience.tsx
+++ src/components/main/Experience.tsx
@@ -6,23 +6,6 @@ export default function Experience (): ReactElement {
   return (
     <div className={`p-2 text-dark-blue`}>
       <div>
-        <h2 className={`text-2xl`}>Software Developer - Intern</h2>
-        <p className={`text-lg p-2`}>November 2022 - Present</p>
-        <a href="https://www.dtcl.co.tz/index">
-          <p className={`flex flex-row items-center text-lg p-2`}>
-            DTCL
-            <TbExternalLink className={`text-dark-blue ml-2`} size={`24px`} />
-          </p>
-        </a>
-        <ul className={`list-disc list-outside text-lg pl-8`}>
-          <li className={`ml-2 py-2 pr-2`}>
-            Pushed for a paradigm shift in app development, from Laravel-only apps to Laravel-Inertia-React apps, for both internal 
-            and client projects. This improved user experience and client satisfaction by 60%.
-          </li>
-        </ul>
-      </div>
-
-      <div>
         <h2 className={`text-2xl`}>Open Source Contibutor</h2>
         <p className={`text-lg p-2`}>June 2022 - Present</p>
         <a href="https://github.com/daniel-hauser/react-organizational-chart">
