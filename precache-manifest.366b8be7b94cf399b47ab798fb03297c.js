244272c57517c8bdcb3e6bd93da83add87da2c09
Kim Jisena <kimjisena@gmail.com>
2023-03-11 03:48:22 +0300

    Updates

---
+++ precache-manifest.366b8be7b94cf399b47ab798fb03297c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b02593b799bc8694447d",
+    "url": "/resume/static/css/main.5fee618f.chunk.css"
+  },
+  {
+    "revision": "b02593b799bc8694447d",
+    "url": "/resume/static/js/main.b02593b7.chunk.js"
+  },
+  {
+    "revision": "5e57e0a4a612bb0a7fb6",
+    "url": "/resume/static/js/1.5e57e0a4.chunk.js"
+  },
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "b2bdbb099f0f3c5bea8bbb2dd7722c99",
+    "url": "/resume/static/media/kim.b2bdbb09.jpg"
+  },
+  {
+    "revision": "17ca9f812ac20ab55ad12dd3412fba64",
+    "url": "/resume/index.html"
+  }
+];
